a7deb0993c7eddcbf4937c1d22e115c021973ab7
mnyu01 <mn3305@nyu.edu>
2023-03-04 14:30:51 -0600

    q

--- smtpClient.py
+++ smtpClient.py
@@ -1,8 +1,8 @@
 import socket
 from socket import *
 def smtp_client(port=1025, mailserver='127.0.0.1'):
-    msg = "\r\n Thank you NYU :)"
-    endmsg = "QUIT\r\nQUIT\r\n"
+    msg = "\r\n Thank you NYU :) QUIT \r\n"
+    endmsg = "QUIT\r\n\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
